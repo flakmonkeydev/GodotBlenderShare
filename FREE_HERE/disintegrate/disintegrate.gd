b7527e7e4b525643fe50eb44ef8557c8378b81c7
nguyentruongtuan1111 <95686891+nguyentruongtuan1111@users.noreply.github.com>
2026-03-03 16:26:26 +0700

    dissolve vfx

--- FREE_HERE/disintegrate/disintegrate.gd
+++ FREE_HERE/disintegrate/disintegrate.gd
@@ -1,11 +1,15 @@
 @tool
 extends Node3D
-@export var mesh: MeshInstance3D
-@export var particle: GPUParticles3D
+@export var meshes: Array[MeshInstance3D]
+@export var particles: Array[GPUParticles3D]
+@export var delay_emit_time: float = 0.5
+
+
 @export var duration: float = 1.5:
 	set(val):
 		duration = val
-		particle.lifetime = val
+		for particle in particles:
+			particle.lifetime = val
 
 @export var disintegrate: bool = false:
 	set(val):
@@ -13,15 +17,18 @@ extends Node3D
 		_disintegrate(val)
 		
 func _disintegrate(val: bool) -> void:
-	if mesh and particle:
-		var tween = create_tween()
-		if val:
-			tween.tween_property(mesh, "instance_shader_parameters/dissolve_amount", 1.1, duration).set_ease(Tween.EASE_IN)
-			await get_tree().create_timer(duration / 3.0).timeout
-			particle.visible = true
-			particle.emitting = true
-		else:
-			tween.tween_property(mesh, "instance_shader_parameters/dissolve_amount", -0.1, 0.1)
-			particle.visible = false
-			particle.restart()
-			particle.emitting = false
+	if meshes and particles:
+		for mesh: MeshInstance3D in meshes:
+			var tween = create_tween().set_parallel(true)
+			if val:
+				tween.tween_property(mesh, "instance_shader_parameters/dissolve_amount", 1.1, duration).set_ease(Tween.EASE_OUT)
+				for particle in particles:
+					tween.tween_property(particle, "visible", true, 0.05).set_delay(delay_emit_time)
+					tween.tween_property(particle, "emitting", true, 0.05).set_delay(delay_emit_time)
+
+			else:
+				tween.tween_property(mesh, "instance_shader_parameters/dissolve_amount", -0.1, 0.1)
+				for particle in particles:
+					particle.visible = false
+					particle.restart()
+					particle.emitting = false
