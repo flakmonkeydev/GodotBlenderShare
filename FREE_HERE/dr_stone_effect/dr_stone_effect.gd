49b930222398724679d28d8f2e4fd075e92ec6b4
nguyentruongtuan1111 <nguyentruongtuan1111@gmail.com>
2026-03-13 09:41:41 +0700

    dr stone effect

--- FREE_HERE/dr_stone_effect/dr_stone_effect.gd
+++ FREE_HERE/dr_stone_effect/dr_stone_effect.gd
@@ -1,6 +1,9 @@
 @tool
 extends Node3D
 @export var character: Node3D
+@onready var button_state: Button = $Control/MarginContainer/HBoxContainer/ButtonState
+@onready var button_edge: Button = $Control/MarginContainer/HBoxContainer/ButtonEdge
+@onready var button_effect: Button = $Control/MarginContainer/HBoxContainer/ButtonEffect
 
 @export var is_idle: bool = false:
 	set(val):
@@ -37,3 +40,26 @@ func unfreeze() -> void:
 	character.motion_unfreeze()
 	var tween = create_tween()
 	tween.tween_property(character.mesh, "instance_shader_parameters/dissolve_amount", 1.0, character.linger_time)
+
+
+func _on_button_effect_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		freeze()
+		button_effect.text = "Stop Effect"
+	else:
+		button_effect.text = "Start Effect"
+		unfreeze()
+			
+
+func _on_button_state_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		character.animation_tree.set("parameters/Transition/transition_request", "state_0")
+	else:
+		character.animation_tree.set("parameters/Transition/transition_request", "state_1")
+
+
+func _on_button_edge_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		character.mesh.set_instance_shader_parameter("edge_mode", 1)
+	else:
+		character.mesh.set_instance_shader_parameter("edge_mode", 0)
